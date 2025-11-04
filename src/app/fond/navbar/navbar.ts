031c425b4d2b6e743b3f596ad9241d42fae9c8c5
HisseinHD <hisseindaoud6627@gmail.com>
2025-11-04 09:13:37 +0100

    ajout de traduction

--- src/app/fond/navbar/navbar.ts
+++ src/app/fond/navbar/navbar.ts
@@ -6,10 +6,11 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './navbar.html',
-  styleUrls: ['./navbar.css'], // petite correction : styleUrls au pluriel
+  styleUrls: ['./navbar.css'],
 })
 export class Navbar {
   menuOpen = false;
+  currentLang = 'fr';
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -17,5 +18,23 @@ export class Navbar {
 
   closeMenu() {
     this.menuOpen = false;
+  }
+
+  setLanguage(lang: string) {
+    this.currentLang = lang;
+
+    const select = document.querySelector('select.goog-te-combo') as HTMLSelectElement;
+    if (select) {
+      select.value = lang;
+      select.dispatchEvent(new Event('change'));
+    }
+
+    if (lang === 'ar') {
+      document.documentElement.setAttribute('dir', 'rtl');
+      document.body.style.textAlign = 'right';
+    } else {
+      document.documentElement.setAttribute('dir', 'ltr');
+      document.body.style.textAlign = 'left';
+    }
   }
 }
