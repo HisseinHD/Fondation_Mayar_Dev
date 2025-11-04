74a0b65175ab4f8cc0f635ab37ca31a6facbf4b1
HisseinHD <hisseindaoud6627@gmail.com>
2025-11-04 11:46:59 +0100

    perf:modifficattion build

--- src/app/services/accueil.ts
+++ src/app/services/accueil.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../environments/ environment';  // ← import environment
+import { environment } from '../../environments/environment'; // ← import environment
 
 export interface ContactForm {
   name: string;
